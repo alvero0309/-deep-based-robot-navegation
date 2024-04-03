b2ed108c54d70fbd2e770f1a904edf9c2fb5a08a
alvero0309 <91089309+alvero0309@users.noreply.github.com>
2024-04-03 12:11:44 -0600

    Delete catkin_ws/build directory

--- catkin_ws/build/robotino_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/milena/DRL-robot-navigation/catkin_ws/src/robotino_description
-# Build directory: /home/milena/DRL-robot-navigation/catkin_ws/build/robotino_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
